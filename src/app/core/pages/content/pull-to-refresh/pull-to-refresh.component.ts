a2e62d9039d62294de0e50025cdfbfd0d72f3afd
kouamecorneille <smcorneille@gmail.com>
2024-06-13 08:33:08 +0000

    adding pull to refresh pwa

--- src/app/core/pages/content/pull-to-refresh/pull-to-refresh.component.ts
+++ src/app/core/pages/content/pull-to-refresh/pull-to-refresh.component.ts
@@ -1,9 +1,10 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-pull-to-refresh',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './pull-to-refresh.component.html',
   styleUrl: './pull-to-refresh.component.css'
 })
@@ -13,11 +14,63 @@ export class PullToRefreshComponent {
   isRefreshing = false;
   touchStartY = 0;
   touchMoveY = 0;
+  pStart = { x: 0, y: 0 };
+  pStop = { x: 0, y: 0 };
 
-  constructor() { }
+  constructor() {
+    document.addEventListener("touchstart", (e) => { this.swipeStart(e) });
+    document.addEventListener("touchend", (e) => { this.swipeEnd(e) });
+   }
 
   ngOnInit() {
 
+  }
+
+  swipeStart(e: any) {
+    if (typeof e["targetTouches"] !== "undefined") {
+      const touch = e.targetTouches[0];
+      this.pStart.x = touch.screenX;
+      this.pStart.y = touch.screenY;
+    } else {
+      this.pStart.x = e.screenX;
+      this.pStart.y = e.screenY;
+    }
+  }
+
+  swipeEnd(e: any) {
+    if (typeof e["changedTouches"] !== "undefined") {
+      const touch = e.changedTouches[0];
+      this.pStop.x = touch.screenX;
+      this.pStop.y = touch.screenY;
+    } else {
+      this.pStop.x = e.screenX;
+      this.pStop.y = e.screenY;
+    }
+
+    this.swipeCheck();
+  }
+
+  swipeCheck() {
+    const changeY = this.pStart.y - this.pStop.y;
+    const changeX = this.pStart.x - this.pStop.x;
+    if (this.isPullDown(changeY, changeX)) {
+      // do something to refresh / call apis to refresh data
+      this.refreshPage();
+    }
+  }
+
+  isPullDown(dY: number, dX: number) {
+    // methods of checking slope, length, direction of line created by swipe action
+    return (
+      dY < 0 &&
+      ((Math.abs(dX) <= 100 && Math.abs(dY) >= 100) ||
+        (Math.abs(dX) / Math.abs(dY) <= 0.3 && dY >= 60))
+    );
+  }
+
+  ngOnDestroy(): void {
+    document.removeEventListener('touchstart', (e) => { this.swipeStart(e); });
+    document.removeEventListener("touchend", (e) => { this.swipeEnd(e); });
   }
 
 
